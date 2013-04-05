7fa44d4bf10e6489b443732c921c52e1b76f92c8
Joakim Lundin <joakim.lundin@su.se>
2013-04-05 15:02:26 +0200

    MOB-45 - Simplest header injection

--- assets/www/js/views/app-view.js
+++ assets/www/js/views/app-view.js
@@ -6,6 +6,17 @@ var AppView = Backbone.View.extend({
   openInAppBrowser: function (event) {
     event.preventDefault();
     var url = event.currentTarget.href;
-    window.open(url, '_blank', 'location=no');
+    var ref = window.open(url, '_blank', 'location=no');
+
+    ref.addEventListener('loadstop', function () {
+      cordova.exec(null, null, "InAppBrowser", "injectScriptCode", [
+        "var header = document.createElement('div');" +
+            "header.innerHTML='<h1>SISU</h1>';" +
+            "header.style.top='0';" +
+            "header.style.position='relative';" +
+            "var body = document.getElementsByTagName('body')[0];" +
+            "body.insertBefore(header, body.firstChild);"
+      ]);
+    });
   }
 });
