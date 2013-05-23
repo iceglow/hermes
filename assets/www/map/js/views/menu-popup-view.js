ecbcd8c47ad0cad10e5a40e610e2b9ab3ccb530f
Joakim Lundin <joakim.lundin@su.se>
2013-05-23 07:59:48 +0200

    AppView depends on MenuPopupView

--- assets/www/map/js/views/menu-popup-view.js
+++ assets/www/map/js/views/menu-popup-view.js
@@ -37,90 +37,94 @@
  * @author <a href="mailto:joakim.lundin@su.se">Joakim Lundin</a>
  * @type {Backbone.View}
  */
-var MenuPopupView = Backbone.View.extend(
-    /** @lends MenuPopupView */
-    {
+define([
+  'backbone'
+], function (Backbone, MapModel, MapView, SearchView, MenuPopupView) {
+  return Backbone.View.extend(
+      /** @lends MenuPopupView */
+      {
 
-      /**
-       * @constructs
-       * @param campuses A list of campuses to display.
-       */
-      initialize: function (options) {
-        _.bindAll(this, "render", "selectCampus", "updateCampuses");
+        /**
+         * @constructs
+         * @param campuses A list of campuses to display.
+         */
+        initialize: function (options) {
+          _.bindAll(this, "render", "selectCampus", "updateCampuses");
 
-        this.campuses = options.campuses;
+          this.campuses = options.campuses;
 
-        // Calculate header size & position menupopup beneath
-        var header = $('div[data-role="header"]');
-        var margin = header.size() > 0 ? (header.position().top + header.height()) : 0;
+          // Calculate header size & position menupopup beneath
+          var header = $('div[data-role="header"]');
+          var margin = header.size() > 0 ? (header.position().top + header.height()) : 0;
 
-        $('<style>')
-            .text('.menupopup-margin { margin-top: ' + Math.round(margin) + 'px; }')
-            .appendTo('head');
-        this.$el.parent().addClass('menupopup-margin');
-        this.$el.parent().addClass('menupopup');
+          $('<style>')
+              .text('.menupopup-margin { margin-top: ' + Math.round(margin) + 'px; }')
+              .appendTo('head');
+          this.$el.parent().addClass('menupopup-margin');
+          this.$el.parent().addClass('menupopup');
 
-        this.campuses.on("reset", this.updateCampuses, this);
+          this.campuses.on("reset", this.updateCampuses, this);
 
-        // Popup state reflects in button styling
-        this.$el.on("popupafteropen", this.buttonPress);
-        this.$el.on("popupafterclose", this.buttonUnpress);
-      },
+          // Popup state reflects in button styling
+          this.$el.on("popupafteropen", this.buttonPress);
+          this.$el.on("popupafterclose", this.buttonUnpress);
+        },
 
-      /** Registers events */
-      events: {
-        "click #menupopupList": "selectCampus"
-      },
+        /** Registers events */
+        events: {
+          "click #menupopupList": "selectCampus"
+        },
 
-      /**
-       * Render the campus popup view.
-       */
-      render: function () {
-        // close any other open popup (only one popup can be open at the same time.)
-        $(document).find("[data-role='popup']:not([id='menupopup'])").each(function () {
-          $(this).popup("close");
-        });
+        /**
+         * Render the campus popup view.
+         */
+        render: function () {
+          // close any other open popup (only one popup can be open at the same time.)
+          $(document).find("[data-role='popup']:not([id='menupopup'])").each(function () {
+            $(this).popup("close");
+          });
 
-        this.$el.popup("open");
-      },
+          this.$el.popup("open");
+        },
 
-      /**
-       * Triggered by a campus selection. Sets the campus in the map app & closes the popup.
-       *
-       * @param evt the event
-       */
-      selectCampus: function (evt) {
-        // get the campus id from the parent <li> (format "campus-X", where X is a number)
-        var campusId = $(evt.target).closest('li').get(0).id.split("campus-")[1];
-        this.trigger('selected', this.campuses.get(campusId));
+        /**
+         * Triggered by a campus selection. Sets the campus in the map app & closes the popup.
+         *
+         * @param evt the event
+         */
+        selectCampus: function (evt) {
+          // get the campus id from the parent <li> (format "campus-X", where X is a number)
+          var campusId = $(evt.target).closest('li').get(0).id.split("campus-")[1];
+          this.trigger('selected', this.campuses.get(campusId));
 
-        this.$el.popup('close');
-      },
+          this.$el.popup('close');
+        },
 
-      /**
-       * Refreshes the campus list in the popup.
-       */
-      updateCampuses: function () {
-        // remove everything from the list
-        $("#menupopupList").find("li").remove();
+        /**
+         * Refreshes the campus list in the popup.
+         */
+        updateCampuses: function () {
+          // remove everything from the list
+          $("#menupopupList").find("li").remove();
 
-        // append all campuses
-        this.campuses.each(function (campus) {
-          $("#menupopupList").append("<li id='campus-" + campus.get('id') + "' data-icon='false' data-theme='b'><a href='javascript://nop'>" + campus.get('name') + "</a></li>");
-        });
+          // append all campuses
+          this.campuses.each(function (campus) {
+            $("#menupopupList").append("<li id='campus-" + campus.get('id') + "' data-icon='false' data-theme='b'><a href='javascript://nop'>" + campus.get('name') + "</a></li>");
+          });
 
-        $("#menupopupList").listview();
-        $("#menupopupList").listview("refresh"); // jQuery mobile-ify the added elements
-      },
+          $("#menupopupList").listview();
+          $("#menupopupList").listview("refresh"); // jQuery mobile-ify the added elements
+        },
 
-      /**
-       * Changes button to pressed or unpressed
-       */
-      buttonPress: function () {
-        $(document).find("a#menubutton").addClass('selected');
-      },
+        /**
+         * Changes button to pressed or unpressed
+         */
+        buttonPress: function () {
+          $(document).find("a#menubutton").addClass('selected');
+        },
 
-      buttonUnpress: function () {
-        $(document).find("a#menubutton").removeClass('selected');
-      }
-    });
+        buttonUnpress: function () {
+          $(document).find("a#menubutton").removeClass('selected');
+        }
+      });
+});
