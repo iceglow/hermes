f9ca476dc6b96ca5fa52dc5db975fb6c498c1c55
Joakim Lundin <joakim.lundin@su.se>
2013-05-24 14:49:30 +0200

    Fixes to tests

--- assets/www/spec/spec-runner.js
+++ assets/www/spec/spec-runner.js
@@ -38,7 +38,6 @@ require.config({
     jquery: 'js/lib/jquery-1.8.2.min',
     jquery_mobile: 'js/lib/jquery.mobile-1.3.1.min',
     jquery_mobile_config: 'js/jquery.mobile-config',
-    jquery_ui_map: 'map/js/lib/jquery.ui.map/jquery.ui.map.min',
     underscore: 'js/lib/underscore-1.4.4-min',
     backbone: 'js/lib/backbone-1.0.0-min',
     i18n: 'js/lib/i18next-1.6.2.min',
